b2b6e71a98ece61a7e2cb8b2414a1d1b1b3a5744
Griffin <griffinlebeau2@gmail.com>
2022-05-19 19:55:55 -0500

    changes

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-var boxHigh = document.querySelector("#uppercase");
-var boxLow = document.querySelector("#lowercase");
-var boxNum = document.querySelector("#number");
-var boxSpec = document.querySelector("#special");
+var boxHigh = document.querySelector("#uppercase").checked;
+var boxLow = document.querySelector("#lowercase").checked;
+var boxNum = document.querySelector("#number").checked;
+var boxSpec = document.querySelector("#special").checked;
 var generateBtn = document.querySelector("#generate")
-var passwordText = document.querySelector("#password");
+var passwordText = document.querySelector("#password").innerHTML;
 var passLength = document.querySelector("#length");
 var low = "abcdefghijklmnopqrstuvwxyz";
 var high = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
 var spec = "!@#$%^&*";
 var password = "";
+var string = "";
 
-boxLow.check
-}
+function generateString() {
+    if (boxLow = true) {
+        string + low;
+    };
+    if (boxHigh = true) {
+        string + high;
+    };
+    if (boxNum = true) {
+        string + num;
+    };
+    if (boxSpec = true) {
+        string + spec;
+    };
+    console.log(string);
+    return string;
+};
 
-//generate password
+function generatePassword() {
+    for (i = 0; i < length; i++){
+    password += string.charAt(
+        Math.floor(Math.random() * length));
+    };
+    return password
+};
 
-//write password
+function writePassword() {
+    generateString();
+    generatePassword();
+    passwordText = password 
+    return passwordText
+};
 
-//button event listen
-
+generateBtn.addEventListener("click", writePassword)
