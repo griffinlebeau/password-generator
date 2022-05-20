6d8354663640a04f90282ea46b234667b9fadac8
Griffin <griffinlebeau2@gmail.com>
2022-05-20 00:00:44 -0500

    final changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var boxHigh = document.querySelector("#uppercase").checked;
-var boxLow = document.querySelector("#lowercase").checked;
-var boxNum = document.querySelector("#number").checked;
-var boxSpec = document.querySelector("#special").checked;
+var boxHigh = document.querySelector("#uppercase");
+var boxLow = document.querySelector("#lowercase");
+var boxNum = document.querySelector("#numbers");
+var boxSpec = document.querySelector("#special");
 var generateBtn = document.querySelector("#generate")
-var passwordText = document.querySelector("#password").innerHTML;
-var passLength = document.querySelector("#length");
+var passwordText = document.querySelector("#password");
+var passLength = document.querySelector("#length").value;
 var low = "abcdefghijklmnopqrstuvwxyz";
 var high = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
@@ -12,36 +12,42 @@ var spec = "!@#$%^&*";
 var password = "";
 var string = "";
 
+
 function generateString() {
-    if (boxLow = true) {
-        string + low;
+    if (boxLow.checked) {
+        string += low;
     };
-    if (boxHigh = true) {
-        string + high;
+    if (boxHigh.checked) {
+        string += high;
     };
-    if (boxNum = true) {
-        string + num;
+    if (boxNum.checked) {
+        string += num;
     };
-    if (boxSpec = true) {
-        string + spec;
+    if (boxSpec.checked) {
+        string += spec;
     };
     console.log(string);
     return string;
 };
 
 function generatePassword() {
-    for (i = 0; i < length; i++){
-    password += string.charAt(
-        Math.floor(Math.random() * length));
-    };
-    return password
+    if (string === ""){
+        window.alert("You must select a box!");
+    } else { 
+        password = "";
+        for (var i = 0; i <= passLength; i++) {
+            var randomNumber = Math.round(Math.random() * string.length);
+            password += string.charAt(randomNumber);
+            console.log(password);
+        };
+    }
+
 };
 
 function writePassword() {
-    generateString();
-    generatePassword();
-    passwordText = password 
-    return passwordText
+    generatePassword(generateString());
+    passwordText.value = password; 
+    return passwordText;
 };
 
 generateBtn.addEventListener("click", writePassword)
