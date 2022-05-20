2c3a78851ad4cc068fadf1a4dc48d2b198648eb2
Griffin <griffinlebeau2@gmail.com>
2022-05-19 19:06:41 -0500

    changes

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-// Assignment code here
+var boxHigh = document.querySelector("#uppercase");
+var boxLow = document.querySelector("#lowercase");
+var boxNum = document.querySelector("#number");
+var boxSpec = document.querySelector("#special");
+var generateBtn = document.querySelector("#generate")
+var passwordText = document.querySelector("#password");
+var passLength = document.querySelector("#length");
+var low = "abcdefghijklmnopqrstuvwxyz";
+var high = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var spec = "!@#$%^&*";
+var password = "";
 
-var numbers = "0123456789";
-var passLength = document.getElementById("slider");
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var special = "!@#$%^&*";
-var box1 = document.getElementById("uppercase");
-var box2 = document.getElementById("lowercase");
-var box3 = document.getElementById("numbers");
-var box4 = document.getElementById("special");
-var password = "boobs";
-
-box2.addEventListener("click", addUppercase);
-function addUppercase() {}
-
-box3.addEventListener("click", addNumbers);
-function addNumbers() {}
-
-box4.addEventListener("click", addSpecial);
-function addSpecial() {}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
-
-function writePassword() {
-  var password = document.querySelector("#password");
-  }
-
-function generatePassword() {
-  for (var i = 0; i < passLength; i++) {
-    password += characters.charAt(
-    Math.floor(Math.random() * passLength.value));
-    }
-    return password;
+boxLow.check
 }
 
+//generate password
 
+//write password
 
+//button event listen
 
